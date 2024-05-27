39ff03c13ab8940813e3b51fb13c3ed6f0175c51
Khalil Aliev <khalilaliev@Khalil.local>
2024-05-27 12:56:24 +0200

    added registration form

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      custom: "#343434",
+    },
     extend: {
       container: {
         padding: "2rem",
