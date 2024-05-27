ce4294117dabe2e18c49968eea1a3d8bb6de1dde
Khalil Aliev <khalilaliev@Khalil.local>
2024-05-27 12:05:45 +0200

    first

---
+++ main.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = /*html*/ `
+<div class="navbar">
+<div class="navbar-start">
+  <a class="navbar-item">Hillel</a>
+</div>
+<div class="navbar-center">
+  <a class="navbar-item navbar-active">Registration form</a>
+</div>
+</div>
+`;
