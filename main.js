d90c5f955ea5cc91fc2e08eed2f1f88a3a020ae7
Khalil Aliev <khalilaliev@Khalil.local>
2024-05-28 15:40:52 +0200

    added data to table

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
+// main.js
 import "./style.css";
 
-// const saveBtn = document.getElementById("saveBtn");
-// const form = document.getElementById("formBox");
-// const table = document.getElementById("table");
-// const male = document.getElementById("male");
-// const female = document.getElementById("female");
+const btn = document.getElementById("saveBtn");
+const table = document.getElementById("table");
+const form = document.getElementById("formBox");
+const infoBox = document.getElementById("infoBox");
+table.style.display = "none";
 
-// // table.style.display = "none";
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
 
-// const getId = (id) => {
-//   return document.getElementById(id);
-// };
+  if (!form.checkValidity()) {
+    return;
+  }
 
-// console.log(getId("name"));
+  const firstName = document.getElementById("userName").value;
+  const lastName = document.getElementById("lastName").value;
+  const dateOfBirth = document.getElementById("dateOfBirth").value;
+  const address = document.getElementById("textarea").value;
+  const city = document.getElementById("cityInput").value;
 
-// saveBtn.addEventListener("click", () => {
-//   const name = document.querySelectorAll(".userName");
-//   getId("name").innerHTML = name.value;
-// });
+  let selectedLanguage = "Not specified";
+  document.querySelectorAll("input[type=checkbox]").forEach((checkbox) => {
+    if (checkbox.checked) {
+      selectedLanguage = checkbox.nextElementSibling.textContent;
+    }
+  });
+
+  let gender;
+  document.querySelectorAll("input[type=radio]").forEach((radio) => {
+    if (radio.checked) {
+      gender = radio.nextElementSibling.textContent;
+    }
+  });
+
+  document.getElementById("name").innerText = firstName;
+  document.getElementById("surname").innerText = lastName;
+  document.getElementById("birth").innerText = dateOfBirth;
+  document.getElementById("address").innerText = address;
+  document.getElementById("city").innerText = city;
+  document.getElementById("language").innerText = selectedLanguage;
+  document.getElementById("gender").innerText = gender;
+  infoBox.style.display = "none";
+  table.style.display = "block";
+});
